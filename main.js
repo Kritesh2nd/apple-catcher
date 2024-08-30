ecfc6f9061ee5be5017ca70a242e07f13fb8fcd5
kritesh2nd <kritesh2nd@gmail.com>
2024-08-30 20:57:51 +0545

    fix: some bug

--- main.js
+++ main.js
@@ -32,12 +32,12 @@ class GameScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("bg", "/public/assets/bg.png");
-    this.load.image("basket", "/public/assets/basket.png");
-    this.load.image("apple", "/public/assets/apple.png");
-    this.load.image("money", "/public/assets/money.png");
-    this.load.audio("coin", "/public/assets/coin.mp3");
-    this.load.audio("bgMusic", "/public/assets/bgMusic.mp3");
+    this.load.image("bg", "/apple-catcher/public/assets/bg.png");
+    this.load.image("basket", "/apple-catcher/public/assets/basket.png");
+    this.load.image("apple", "/apple-catcher/public/assets/apple.png");
+    this.load.image("money", "/apple-catcher/public/assets/money.png");
+    this.load.audio("coin", "/apple-catcher/public/assets/coin.mp3");
+    this.load.audio("bgMusic", "/apple-catcher/public/assets/bgMusic.mp3");
   }
   create() {
     this.scene.pause("scene-game");
