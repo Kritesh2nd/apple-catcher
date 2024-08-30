2e3e670bf4d17bd72ee43b924be81da823ed9882
kritesh2nd <kritesh2nd@gmail.com>
2024-08-30 20:53:24 +0545

    fix: some bug

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [react()],
-   base: '/apple-catcher/'
-})
+  // GitHub Pages expects an index.html in the root directory
+  // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
+//   build: { outDir: '..' },
+  // needed for github pages just put the repo name here
+  base: '/apple-catcher/', 
+});
